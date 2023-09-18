fedbf6f3516119032874d1544448b95c5feb808d
Maryam Noori <maryamnoori@Maryams-MacBook-Pro.local>
2023-09-18 22:31:25 +0200

    First commit

---
+++ Lektion_1_intro/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    // PSVM
+    public static void main(String[] args) {
+
+        // sout
+        System.out.println("Hello World!");
+
+        // Variabels
+        // datatype name = value
+        int number = 777;
+        System.out.println("our variable 'number' is: " + number);
+        /*
+        * Array börjar alltid med noll med for loop kan börja med vilket som
+        */
+
+
+
+
+    }
+}
+/**/
+
